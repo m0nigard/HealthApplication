969c93a93e58b2ef17a12bb223a06e125e41b034
m0nigard <mans.thomasson2@gmail.com>
2022-05-16 14:20:09 +0200

    first commit, added RestServer.Js

---
+++ backend/RestServer.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var app = express()
+
+var bodyParser = require('body-parser');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const SERVER_PORT = process.env.PORT || 3000;
+const MAX_LENGTH_WORD = 10;
+
+app.post('/data', function (req, res) {
+  console.log("post requested received with data: ");
+
+  var data = req.body.data;
+
+  console.log(data);
+
+  console.log("sending response");
+  res.send(data);
+  res.end();
+
+})
+
+app.listen(SERVER_PORT, () => {
+  console.log("Server listening on port: " + SERVER_PORT);
+})
