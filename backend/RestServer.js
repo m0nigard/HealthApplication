fd1bac6a76e7bb8abc55e91159534429d338c05e
B0gg3 <mats_06_20@hotmail.com>
2022-05-17 13:46:24 +0200

    fixed getting data

--- backend/RestServer.js
+++ backend/RestServer.js
@@ -17,12 +17,15 @@ const MAX_LENGTH_WORD = 10;
 app.post('/data', function (req, res) {
   console.log("post requested received with data: ");
 
-  var data = req.body.data;
+  var data = req.body.data.metrics;
 
-  console.log(data);
+  console.log(data)
+
+  var myData = []
+  myData = getData(data) // functions puts steps in index 0, index 1 distance walked and run and index 2 is walking pace.
 
   console.log("sending response");
-  res.send(data);
+  res.send("data");
   res.end();
 
 })
@@ -30,3 +33,22 @@ app.post('/data', function (req, res) {
 app.listen(SERVER_PORT, () => {
   console.log("Server listening on port: " + SERVER_PORT);
 })
+
+function getData(data){
+  var myData = []
+  var filteredData = []
+
+  var resArr = data.map(function(x){ // make data map and push everything.
+    myData.push(x[Object.keys(x)[0]]) // data is not in the same spot so everything needs to be pushed.
+    myData.push(x[Object.keys(x)[1]])
+    myData.push(x[Object.keys(x)[2]])
+  });
+
+  for(var p = 0; p< myData.length; p++){ // get all the data
+    if(Array.isArray(myData[p])){ // if the data is an arrray
+      filteredData.push(myData[p][0]) // push the dictionary on to the new array
+
+    }
+  }
+  return filteredData 
+}
